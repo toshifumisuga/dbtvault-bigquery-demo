5ed3221615c9d89a3a98ddde81fac67db0e83182
Toshifumi Suga <toshifumi.suga.122@gmail.com>
2024-07-03 01:11:43 +0900

    インデント修正

--- models/vault/hubs/h_customer.sql
+++ models/vault/hubs/h_customer.sql
@@ -1,8 +1,14 @@
-{%- set source_model = ["stg_customer"] -%}
+{%- set source_model = "stg_customer" -%}
 {%- set src_pk = "CUSTOMER_PK" -%}
 {%- set src_nk = "CUSTOMER_ID" -%}
 {%- set src_ldts = "LOAD_DATE" -%}
 {%- set src_source = "RECORD_SOURCE" -%}
 
-{{ automate_dv.hub(src_pk=src_pk, src_nk=src_nk, src_ldts=src_ldts,
-                   src_source=src_source, source_model=source_model) }}
+{{ automate_dv.hub(
+        src_pk=src_pk,
+        src_nk=src_nk,
+        src_ldts=src_ldts,
+        src_source=src_source,
+        source_model=source_model
+    ) 
+}}
--- models/vault/links/l_customer_order.sql
+++ models/vault/links/l_customer_order.sql
@@ -4,5 +4,11 @@
 {%- set src_ldts = "LOAD_DATE" -%}
 {%- set src_source = "RECORD_SOURCE" -%}
 
-{{ automate_dv.link(src_pk=src_pk, src_fk=src_fk, src_ldts=src_ldts,
-                    src_source=src_source, source_model=source_model) }}
+{{ automate_dv.hub(
+        src_pk=src_pk,
+        src_nk=src_nk,
+        src_ldts=src_ldts,
+        src_source=src_source,
+        source_model=source_model
+    ) 
+}}
